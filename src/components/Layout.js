7993af17208a3707bd67bdfebe04f4dd35f845bf
Adrian Greksa <adrian.greksa@gmail.com>
2022-08-15 12:58:36 +1000

    added react-tinder-card and movies API

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth";
+import '../App.css';
 
 const Layout = ({ children }) => {
     const auth = useAuth();
     return (
         <div>
-            <header>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/profile"}>Profile</Link></li>
-                {auth.user ?
-                    <li>
-                        <button onClick={auth.logout}>Logout</button>
-                    </li>
-                    :
-                    <li><Link to={"/sign-in"}>Sign In</Link></li>
-                }
+            <header className={"header"}>
+                <ul>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/profile"}>Profile</Link></li>
+                    {auth.user ?
+                        <li>
+                            <button onClick={auth.logout}>Logout</button>
+                        </li>
+                        :
+                        <li><Link to={"/sign-in"}>Sign In</Link></li>
+                    }
+                </ul>
             </header>
 
-            <main>
+            <main className={"container"}>
                 {children}
             </main>
         </div>
     );
 };
 
-export default Layout; 
+export default Layout;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout";
+import axios from "axios";
+import TinderCard from "react-tinder-card";
+import MovieCard from "../components/MovieCard";
 
 const Home = () => {
+    const [movies, setMovies] = useState([])
+
+    const fetchMovies = async () => {
+        const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie", {
+            params: {
+                page: Math.random() * 501,
+                api_key: "356fa614caff30a00301706a234ca902"
+            }
+        })
+        // console.log(data);
+        setMovies(data.results)
+    }
+
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+    
+    const renderMovies = () => {
+        return movies.map(movie => (
+            <TinderCard key={movie.id}>
+                <MovieCard movie={movie} />
+            </TinderCard>
+        ))
+    }
+
     return (
         <Layout>
             <h1>Welcome!</h1>
+            <div className="movie-wrapper">
+                {renderMovies()}
+            </div>
+                
+
+
         </Layout>
     );
 };
 
-export default Home; 
+export default Home;
